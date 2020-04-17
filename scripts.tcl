4f9a13b68fee4628764842329b5239f11385a890
Hasbullah Masby <63797095+akoeabi@users.noreply.github.com>
2020-04-17 13:54:31 +0800

    Owned by me

---
+++ scripts.tcl
@@ -0,0 +1,9 @@
+set eggdrop(root) "scripts"
+
+source "$eggdrop(root)/alltools.tcl"
+source "$eggdrop(root)/action.fix.tcl"
+source "$eggdrop(root)/dccwhois.tcl"
+source "$eggdrop(root)/userinfo.tcl"
+#source "$eggdrop(root)/tls.tcl"
+#source "$eggdrop(root)/json.tcl"
+#source "$eggdrop(root)/http.tcl"
